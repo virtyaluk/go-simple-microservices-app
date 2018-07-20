b0d0ba4d3d960b92c4d33dec9ba3e1ada0dac602
virtyaluk <winsatana@ya.ru>
2018-07-20 21:04:51 +0300

    Repository name refactoring.

--- showtimes/data/showtimeRepository.go
+++ showtimes/data/showtimeRepository.go
@@ -7,11 +7,11 @@ import (
   "time"
 )
 
-type ShowtimeRepository struct {
+type ShowTimeRepository struct {
   C *mgo.Collection
 }
 
-func (r *ShowtimeRepository) Create(showtime *models.ShowTime) error {
+func (r *ShowTimeRepository) Create(showtime *models.ShowTime) error {
   obj_id := bson.NewObjectId()
   showtime.Id = obj_id
   showtime.CreatedOn = time.Now()
@@ -20,7 +20,7 @@ func (r *ShowtimeRepository) Create(showtime *models.ShowTime) error {
   return err
 }
 
-func (r *ShowtimeRepository) GetAll() []models.ShowTime {
+func (r *ShowTimeRepository) GetAll() []models.ShowTime {
   var showtimes []models.ShowTime
   iter := r.C.Find(nil).Iter()
   result := models.ShowTime{}
@@ -32,13 +32,13 @@ func (r *ShowtimeRepository) GetAll() []models.ShowTime {
   return showtimes
 }
 
-func (r *ShowtimeRepository) GetByDate(date string) (showtime models.ShowTime, err error) {
+func (r *ShowTimeRepository) GetByDate(date string) (showtime models.ShowTime, err error) {
   err = r.C.Find(bson.M{"date": date}).One(&showtime)
 
   return
 }
 
-func (r *ShowtimeRepository) Delete(id string) error {
+func (r *ShowTimeRepository) Delete(id string) error {
   err := r.C.Remove(bson.M{"_id": bson.ObjectIdHex(id)})
 
   return err
