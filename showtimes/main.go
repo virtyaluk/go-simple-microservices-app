34710a847b4850f13a256c031ae79bd22646881d
virtyaluk <winsatana@ya.ru>
2018-07-20 21:38:26 +0300

    Updates services' entry point.

--- showtimes/main.go
+++ showtimes/main.go
@@ -1,6 +1,24 @@
 package main
 
+import (
+  "net/http"
+  "log"
+  "github.com/virtyaluk/go-simple-microservices-app/showtimes/common"
+  "github.com/virtyaluk/go-simple-microservices-app/showtimes/routers"
+)
+
 // Entry point for the program
 func main() {
+  // Calls startup logic
+  common.StartUp()
+  // Get the mus router object
+  router := routers.InitRoutes()
 
+  server := &http.Server{
+    Addr: common.AppConfig.Server,
+    Handler: router,
+  }
+
+  log.Println("Listening...")
+  server.ListenAndServe()
 }
