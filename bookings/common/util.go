21221ef01537f37e952807a5728eb2b9d2feca4c
virtyaluk <winsatana@ya.ru>
2018-07-20 03:54:42 +0300

    Adds common utils.

---
+++ bookings/common/util.go
@@ -0,0 +1,100 @@
+package common
+
+import (
+  "net/http"
+  "log"
+  "github.com/gin-gonic/gin/json"
+  "os"
+  "gopkg.in/mgo.v2"
+  "time"
+)
+
+type (
+  appError struct {
+    Error      string `json:"error"`
+    Message    string `json:"message"`
+    HttpStatus int    `json:"status"`
+  }
+
+  errorResource struct {
+    Data appError `json:"data"`
+  }
+
+  configuration struct {
+    Server, MongoDbHost, DBUser, DBPwd, Database string
+  }
+)
+
+func DisplayAppError(w http.ResponseWriter, handlerError error, message string, code int) {
+  errObj := appError{
+    Error: handlerError.Error(),
+    Message: message,
+    HttpStatus: code,
+  }
+
+  log.Printf("AppError]: %s\n", handlerError)
+  w.Header().Set("Content-Type", "application/json; charset=utf-8")
+  w.WriteHeader(code)
+
+  if j, err:= json.Marshal(errorResource{Data: errObj}); err == nil {
+    w.Write(j)
+  }
+}
+
+// AppConfig holds the configuration values from config.json file
+var AppConfig configuration
+
+// Initialize AppConfig
+func initConfig() {
+  file, err := os.Open("common/config.json")
+  defer file.Close()
+
+  if err != nil {
+    log.Fatalf("[logAppConfig]: %s\n", err)
+  }
+
+  decoder := json.NewDecoder(file)
+  AppConfig = configuration{}
+  err = decoder.Decode(&AppConfig)
+
+  if err != nil {
+    log.Fatalf("[logAppConfig]: %s\n", err)
+  }
+}
+
+// Session holds the mongodb session for database access
+var session *mgo.Session
+
+// Get database session
+func GetSession() *mgo.Session {
+  if session == nil {
+    var err error
+    session, err = mgo.DialWithInfo(&mgo.DialInfo{
+      Addrs: []string{AppConfig.MongoDbHost},
+      Username: AppConfig.DBUser,
+      Password: AppConfig.DBPwd,
+      Timeout: 60 * time.Second,
+    })
+
+    if err != nil {
+      log.Fatalf("[GetSession]: %s\n", err)
+    }
+  }
+
+  return session;
+}
+
+// Create database session
+func createDbSession() {
+  var err error
+  session, err = mgo.DialWithInfo(&mgo.DialInfo{
+    Addrs: []string{AppConfig.MongoDbHost},
+    Username: AppConfig.DBUser,
+    Password: AppConfig.DBPwd,
+    Timeout: 60 * time.Second,
+  })
+
+  if err != nil {
+    log.Fatalf("[createDbSession]: %s\n", err)
+  }
+}
